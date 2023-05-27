a4fe5d08caafe587cfd042ac821e131cb6d5ebe7
calben99 <calben99@users.noreply.github.com>
2023-05-27 01:30:42 -0700

    Add files via upload

---
+++ fig/src/App.tsx
@@ -0,0 +1,17 @@
+import { memo } from 'react';
+import type { FC } from 'react';
+
+import classes from './App.module.css';
+import resets from './components/_resets.module.css';
+import { Frame203 } from './components/Frame203/Frame203';
+
+interface Props {
+  className?: string;
+}
+export const App: FC<Props> = memo(function App(props = {}) {
+  return (
+    <div className={`${resets.storybrainResets} ${classes.root}`}>
+      <Frame203 />
+    </div>
+  );
+});
