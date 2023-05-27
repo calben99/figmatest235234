edae231bebc3eb573dc0eb0380972f171a4e8983
calben99 <calben99@users.noreply.github.com>
2023-05-27 01:34:30 -0700

    Add files via upload

---
+++ src/components/Frame203/Frame203.tsx
@@ -0,0 +1,81 @@
+import { memo } from 'react';
+import type { FC } from 'react';
+
+import resets from '../_resets.module.css';
+import classes from './Frame203.module.css';
+
+interface Props {
+  className?: string;
+}
+/* @figmaId 6:1334 */
+export const Frame203: FC<Props> = memo(function Frame203(props = {}) {
+  return (
+    <div className={`${resets.storybrainResets} ${classes.root}`}>
+      <div className={classes.iMAGE}></div>
+      <div className={classes.frame206}>
+        <div className={classes.frame205}>
+          <div className={classes.googleXNBA}>Google x NBA</div>
+          <div className={classes.frame204}>
+            <div className={classes.uX}>UX</div>
+            <div className={classes.visual}>Visual</div>
+            <div className={classes.motion}>Motion</div>
+            <div className={classes.designSystem}>Design System</div>
+          </div>
+          <div className={classes.websiteDesign}>Website Design</div>
+          <div className={classes.vector}>
+            <div className={classes.vector2}></div>
+          </div>
+        </div>
+        <div className={classes.rectangle}></div>
+      </div>
+      <div className={classes.frame207}>
+        <div className={classes.frame2052}>
+          <div className={classes.googleXNBA2}>Google x NBA</div>
+          <div className={classes.frame2042}>
+            <div className={classes.uX2}>UX</div>
+            <div className={classes.visual2}>Visual</div>
+            <div className={classes.motion2}>Motion</div>
+            <div className={classes.designSystem2}>Design System</div>
+          </div>
+          <div className={classes.websiteDesign2}>Website Design</div>
+          <div className={classes.vector3}>
+            <div className={classes.vector4}></div>
+          </div>
+        </div>
+        <div className={classes.rectangle2}></div>
+      </div>
+      <div className={classes.frame208}>
+        <div className={classes.frame2053}>
+          <div className={classes.googleXNBA3}>Google x NBA</div>
+          <div className={classes.frame2043}>
+            <div className={classes.uX3}>UX</div>
+            <div className={classes.visual3}>Visual</div>
+            <div className={classes.motion3}>Motion</div>
+            <div className={classes.designSystem3}>Design System</div>
+          </div>
+          <div className={classes.websiteDesign3}>Website Design</div>
+          <div className={classes.vector5}>
+            <div className={classes.vector6}></div>
+          </div>
+        </div>
+        <div className={classes.rectangle3}></div>
+      </div>
+      <div className={classes.frame209}>
+        <div className={classes.frame2054}>
+          <div className={classes.googleXNBA4}>Google x NBA</div>
+          <div className={classes.frame2044}>
+            <div className={classes.uX4}>UX</div>
+            <div className={classes.visual4}>Visual</div>
+            <div className={classes.motion4}>Motion</div>
+            <div className={classes.designSystem4}>Design System</div>
+          </div>
+          <div className={classes.websiteDesign4}>Website Design</div>
+          <div className={classes.vector7}>
+            <div className={classes.vector8}></div>
+          </div>
+        </div>
+        <div className={classes.rectangle4}></div>
+      </div>
+    </div>
+  );
+});
